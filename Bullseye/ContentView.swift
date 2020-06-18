585becb9a8bf9db9e0c43b5d89ed9847b74afa0c
Maggie Yi <myi_gomboc@berkeley.edu>
2020-06-18 10:51:17 -0700

    first commit

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,9 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: data for pop-up boolean values
+    //Indicates that if the body changed, state is in sync
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Greetings from the meme lord")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .italic()
+                .foregroundColor(Color.blue)
+            Button(action: {
+                print("Button pressed")
+                //self refers to ContentView
+                self.alertIsVisible = true
+            }) {
+                Text("Hit Me!")
+                    .font(.callout)
+                    .bold()
+            }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                //()->Alert: takes no parameters and turns into an Alert object
+                //define what the alert is going to be
+                return Alert(title: Text("Do The Fortnite Dance"), message: Text("this is my first pop-up"), dismissButton: .default(Text("Awesome!")))
+                
+            }
+            
+//           Button("Hello World"){
+//               print(type(of: self))
+//           }
+//           .frame(width:200, height: 300)
+//           .background(Color.yellow)
+        }
+            
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
